3334a605a4ddfef9549a4736b68fc005fb0df7a5
Zack Dinello <zacktd15@gmail.com>
2022-11-05 01:19:19 +0530

    Update DoublyLinkedList method insert at

--- src/com/company/DoublyLinkedList.java
+++ src/com/company/DoublyLinkedList.java
@@ -49,7 +49,64 @@ public class DoublyLinkedList <T extends Comparable<T>> {
 
 
     public Node insert(int location, Album album) throws IllegalArgumentException{
+        //Creating node
+        Node newNode = new Node(album);
 
+        //Checking whether our doubly linked list is empty or not
+        if(head == null && location == 1) {
+            head = newNode;
+        }
+        //Execute when the list is not empty
+        else {
+            int size = 0;
+            Node temp = head;
+            while (temp != null){
+                size++;
+            }
+            if(location == size+1){
+
+                //Call lastNode() method to add the node at last in the list
+                tail.next = newNode;
+                //The tail is pointing to the second last node so the newly created node's prev will point to tail
+                newNode.previous = tail;
+                //The newly created node will become new tail because it is last node in the doubly linked list
+                tail = newNode;
+                //The newly created node will be the last node so tail's next will be null
+                tail.next = null;
+
+            }else if(location == 1){
+
+                //The head's prev will point to the newNode
+                head.previous = newNode;
+                //The newNode's next will point to the head
+                newNode.next = head;
+                //The newNode's prev will point to null because it will be the first node
+                newNode.previous = null;
+                //The newNode will become new head because now the newly created node is the first node of the list
+                head = newNode;
+
+            }else{
+                //current node will point to head
+                Node current = head;
+                Node temp1 = null;
+
+                //Iterate list till current points to the specified position
+                for(int i = 1; i < location-1; i++){
+                    current = current.next;
+                }
+
+                //The temp node points to the node that is next to current
+                temp1 = current.next;
+                temp.previous = current;
+
+                //newNode will be added between current and temp
+                current.next = newNode;
+                newNode.previous = current;
+                newNode.next = temp1;
+                temp1.previous = newNode;
+            }
+        }
+        return newNode;
 
     }
 

a6e6841f1b6ad7416c827b2f71580fee8d87797f
Zack Dinello <zacktd15@gmail.com>
2022-11-06 00:34:53 +0530

    Updated something

--- src/com/company/DoublyLinkedList.java
+++ src/com/company/DoublyLinkedList.java
@@ -11,207 +11,199 @@ public class DoublyLinkedList <T extends Comparable<T>> {
 
 
     public DoublyLinkedList() {
-        this.head = null;
-        this.tail = null;
+        head = null;
+        tail = null;
     }
 
-
-    public Node append(Album album) {
-
-        //create a new node
-        Node toAppend = new Node(album);
-
-        //check if list is empty
-        if (head == null) {
-            head = toAppend;
-            tail = toAppend;
-        }
-
-        //if list length is one or more than one
-        else {
-            toAppend.previous = tail;
-            tail.next = toAppend;
-            tail = tail.next;
-        }
-
-
-        //return node
-        return toAppend;
-    }
-
-
-    public Node delete(int location) throws IllegalArgumentException{
-        Node temp1 = null;
-        int size = 0;
-        Node temp = head;
-        while (temp != null){
-            temp = temp.next;
-            size++;
-        }
-
-        //Check whether the DDL is empty or not
-        if(head == null || location>size) {
-            throw new IllegalArgumentException();
-            //System.out.println("\n" +position +"th position is not available\n");
-        }
-        //Execute when the DDL is not empty
-        else {
-            if(location == 1){
-
-                //Call deleteFirstNode() method to delete first node from the DDL
-                temp1 = head;
-                head = head.next;
-
-                //Decrement the size of DDL
-
-
-            }else if(location == size){
-                //Call deleteLastNode() method to delete last node from the DDL
-                temp1 = tail;
-                tail = tail.previous;
-
-                //Decrement the size of DDL
-
-
-            }else{
-                //current node will point to head
-                Node current = head;
-
-                //Iterate list till current points to the specified position
-                for(int i = 1; i < location; i++){
-                    current = current.next;
-                }
-
-                //Delete node pointed by current
-                temp1  = current;
-                current.next.previous = current.previous;
-                current.previous.next = current.next;
-
-            }
-            return temp1;
-        }
-    }
-
-
+//
+//    public Node append(Album album) {
+//
+//        //create a new node
+//        Node toAppend = new Node(album);
+//
+//        //check if list is empty
+//        if (head == null) {
+//            head = toAppend;
+//            tail = toAppend;
+//        }
+//
+//        //if list length is one or more than one
+//        else {
+//            toAppend.previous = tail;
+//            tail.next = toAppend;
+//            tail = tail.next;
+//        }
+//
+//
+//        //return node
+//        return toAppend;
+//    }
+//
+//
+//    public Node delete(int location) throws IllegalArgumentException{
+//        Node temp1 = null;
+//        int size = 0;
+//        Node temp = head;
+//        while (temp != null){
+//            temp = temp.next;
+//            size++;
+//        }
+//
+//        //Check whether the DDL is empty or not
+//        if(head == null || location>size) {
+//            throw new IllegalArgumentException();
+//            //System.out.println("\n" +position +"th position is not available\n");
+//        }
+//        //Execute when the DDL is not empty
+//        else {
+//            if(location == 1){
+//
+//                //Call deleteFirstNode() method to delete first node from the DDL
+//                temp1 = head;
+//                head = head.next;
+//
+//                //Decrement the size of DDL
+//
+//
+//            }else if(location == size){
+//                //Call deleteLastNode() method to delete last node from the DDL
+//                temp1 = tail;
+//                tail = tail.previous;
+//
+//                //Decrement the size of DDL
+//
+//
+//            }else{
+//                //current node will point to head
+//                Node current = head;
+//
+//                //Iterate list till current points to the specified position
+//                for(int i = 1; i < location; i++){
+//                    current = current.next;
+//                }
+//
+//                //Delete node pointed by current
+//                temp1  = current;
+//                current.next.previous = current.previous;
+//                current.previous.next = current.next;
+//
+//            }
+//            return temp1;
+//        }
+//    }
+//
+//
     public int getIndex(Album album) {
-        int index = 0;
-        Node temp = head;
-        while (temp != null){
-            if(temp.album == album){
-                return index;
-            }
-            temp = temp.next;
+        int i = 1;
+        boolean flag = false;
+        //Node current will point to head
+        Node current = head;
+
+        //Checks whether the list is empty
+        if (head == null) {
+            return -1;
         }
+        while (current != null) {
+            //Compare value to be searched with each node in the list
+            if (current.album == album) {
+                flag = true;
+                break;
+            }
+            current = current.next;
+            i++;
+        }
+        if (flag)
+            return i;
+
     return -1;
-    }
+}
 
 
-    public Node insert(int location, Album album) throws IllegalArgumentException{
-        //Creating node
+    public Node insert(int position,Album album) throws IllegalArgumentException {
+
+        //1. allocate node to new element
         Node newNode = new Node(album);
+        newNode.next = null;
+        newNode.previous = null;
 
-        //Checking whether our doubly linked list is empty or not
-        if(head == null && location == 1) {
+        //2. check if the position is > 0
+        if (position < 1) {
+            throw new IllegalArgumentException();
+        } else if (position == 1) {
+
+            //3. if the position is 1, make new node as head
             head = newNode;
-        }
-        //Execute when the list is not empty
-        else {
-            int size = 0;
-            Node temp = head;
-            while (temp != null){
-                temp = temp.next;
-                size++;
-            }
-            if(location > size){
-                throw new IllegalArgumentException();
-            }
-            if(location == size+1){
 
-                //Call lastNode() method to add the node at last in the list
-                tail.next = newNode;
-                //The tail is pointing to the second last node so the newly created node's prev will point to tail
-                newNode.previous = tail;
-                //The newly created node will become new tail because it is last node in the doubly linked list
-                tail = newNode;
-                //The newly created node will be the last node so tail's next will be null
-                tail.next = null;
+        } else {
 
-            }else if(location == 1){
-
-                //The head's prev will point to the newNode
-                head.previous = newNode;
-                //The newNode's next will point to the head
-                newNode.next = head;
-                //The newNode's prev will point to null because it will be the first node
-                newNode.previous = null;
-                //The newNode will become new head because now the newly created node is the first node of the list
-                head = newNode;
-
-            }else{
-                //current node will point to head
-                Node current = head;
-                Node temp1 = null;
-
-                //Iterate list till current points to the specified position
-                for(int i = 1; i < location-1; i++){
-                    current = current.next;
+            //4. Else, make a temp node and traverse to the
+            //   node previous to the position
+            Node temp = null;
+            temp = head;
+            for (int i = 1; i < position - 1; i++) {
+                if (temp != null) {
+                    temp = temp.next;
+                } else {
+                    throw new IllegalArgumentException();
                 }
+            }
 
-                //The temp node points to the node that is next to current
-                temp1 = current.next;
-                temp1.previous = current;
-
-                //newNode will be added between current and temp
-                current.next = newNode;
-                newNode.previous = current;
-                newNode.next = temp1;
-                temp1.previous = newNode;
+            //5. If the previous node is not null, adjust
+            //   the links
+            if (temp != null) {
+                newNode.previous = temp;
+                if (temp.next != null) {
+                    newNode.next = temp.next;
+                    newNode.next.previous = newNode;
+                    temp.next = newNode;
+                }else {
+                    temp.next = newNode;
+                }
             }
         }
         return newNode;
-
     }
 
-
-    public Node shuffle() {
-        Node temp = head;
-        int randomX = (int) (Math.random() * 10 + 1);
-
-//simply go until the randomX
-        while(randomX-- > 0 && temp.next != null)
-            temp = temp.next;
-
-//remove the Nth node from the list
-        temp.previous.next = temp.next;
-
-        if(temp.next != null)
-            temp.next.previous = temp.previous;
-
-//set it to point to the head
-        temp.next = (head);
-        temp.previous = (null);
-
-//now set the Head to the Nth node we found
-        head = temp;
-        return head;
-    }
-
-
-    public DoublyLinkedList<Album> partition(Album album) {
-        DoublyLinkedList<Album> list = new DoublyLinkedList<>();
-        Node temp = head;
-        while (temp != null) {
-            if (temp.album.compareTo(album) == 1) {
-                list.append(album);
-            } else if (temp.album.compareTo(album) > 1) {
-                list.append(album);
-            }
-                temp = temp.next;
-
-        }
-        return list;
-    }
-
+//
+//    public Node shuffle() {
+//        Node temp = head;
+//        int randomX = (int) (Math.random() * 10 + 1);
+//
+////simply go until the randomX
+//        while(randomX-- > 0 && temp.next != null)
+//            temp = temp.next;
+//
+////remove the Nth node from the list
+//        temp.previous.next = temp.next;
+//
+//        if(temp.next != null)
+//            temp.next.previous = temp.previous;
+//
+////set it to point to the head
+//        temp.next = (head);
+//        temp.previous = (null);
+//
+////now set the Head to the Nth node we found
+//        head = temp;
+//        return head;
+//    }
+//
+//
+//    public DoublyLinkedList<Album> partition(Album album) {
+//        DoublyLinkedList<Album> list = new DoublyLinkedList<>();
+//        Node temp = head;
+//        while (temp != null) {
+//            if (temp.album.compareTo(album) == 1) {
+//                list.append(album);
+//            } else if (temp.album.compareTo(album) > 1) {
+//                list.append(album);
+//            }
+//                temp = temp.next;
+//
+//        }
+//        return list;
+//    }
+//
 
     @Override
     public String toString() {
--- src/com/company/DoublyLinkedListTest.java
+++ src/com/company/DoublyLinkedListTest.java
@@ -8,58 +8,60 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class DoublyLinkedListTest {
 
-    @Test
-    void testAppend() {
-        DoublyLinkedList<Album> dll = new DoublyLinkedList<>();
-        assertNull(dll.head);
-        assertNull(dll.tail);
-
-        ArrayList<String> artistNames = new ArrayList<>();
-        artistNames.add("Artist 1");
-        artistNames.add("Artist 2");
-
-        Album a1 = new Album(1, artistNames, "album 1", 1);
-        assertEquals(1, dll.append(a1).album.numberOfSongs);
-
-        Album a2 = new Album(2, artistNames, "album 2", 2);
-        assertEquals(2, dll.append(a2).album.numberOfSongs);
-
-        Album a3 = new Album(3, artistNames, "album 3", 3);
-        assertEquals(3, dll.append(a3).album.numberOfSongs);
-
-        assertEquals(1, dll.head.album.numberOfSongs);
-
-        assertEquals("1 -> 2 -> 3 -> NULL", dll.toString());
-
-    }
+//    @Test
+//    void testAppend() {
+//        DoublyLinkedList<Album> dll = new DoublyLinkedList<>();
+//        assertNull(dll.head);
+//        assertNull(dll.tail);
+//
+//        ArrayList<String> artistNames = new ArrayList<>();
+//        artistNames.add("Artist 1");
+//        artistNames.add("Artist 2");
+//
+//        Album a1 = new Album(1, artistNames, "album 1", 1);
+//        assertEquals(1, dll.append(a1).album.numberOfSongs);
+//
+//        Album a2 = new Album(2, artistNames, "album 2", 2);
+//        assertEquals(2, dll.append(a2).album.numberOfSongs);
+//
+//        Album a3 = new Album(3, artistNames, "album 3", 3);
+//        assertEquals(3, dll.append(a3).album.numberOfSongs);
+//
+//        assertEquals(1, dll.head.album.numberOfSongs);
+//
+//        assertEquals("1 -> 2 -> 3 -> NULL", dll.toString());
+//
+//    }
 
 
     @Test
     void testGetIndex() {
         DoublyLinkedList<Album> dll = new DoublyLinkedList<>();
-        assertNull(dll.head);
-        assertNull(dll.tail);
+
 
         ArrayList<String> artistNames = new ArrayList<>();
         artistNames.add("Artist 1");
         artistNames.add("Artist 2");
 
+        ArrayList<String> artistNames1 = new ArrayList<>();
+        artistNames1.add("Artist 1");
+        artistNames1.add("Artist 2");
+
         Album a1 = new Album(1, artistNames, "album 1", 1);
         dll.insert(1,a1);
-        assertEquals(1, dll.getIndex(a1));
+        //assertEquals(1, dll.getIndex(a1));
 
-        Album a2 = new Album(2, artistNames, "album 2", 2);
-        dll.insert(1,a2);
+        Album a2 = new Album(2, artistNames, "album 2", 1);
+        dll.insert(2,a2);
         assertEquals(2, dll.getIndex(a2));
 
         Album a3 = new Album(3, artistNames, "album 3", 3);
         dll.insert(3,a3);
         assertEquals(3, dll.getIndex(a3));
 
-        //assertEquals(1, dll.head.album.numberOfSongs);
+        assertEquals(1, dll.head.album.numberOfSongs);
 
-        assertEquals("1 -> 2 -> 3 -> NULL", dll.toString());
-
+        //assertEquals("1 -> NULL", dll.toString());
 
     }
 
@@ -93,7 +95,7 @@ public class DoublyLinkedListTest {
     }
 
     @Test
-    void testtoString() {
+    void testToString() {
 
 
     }
